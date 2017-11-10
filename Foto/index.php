3c6316245adcd424e1450deaa7f72a1e1c179b0d
kburthey <kburthey@gmail.com>
2017-11-10 10:55:56 -0500

    Add files via upload

---
+++ Foto/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>Foto | Capture the Moment</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Lora|Raleway" rel="stylesheet">
+	<link rel="stylesheet" href="foto.css">
+  </head>
+	<body>
+
+	<!-- The main section -->
+		<div class="main">
+		  <div class="container">
+			<h1 style="text-align: left">
+			  Introducing Foto
+			</h1>
+			<p>
+			  Capture the moments that matter.
+			</p>
+			<button class="btn btn-danger">Download Foto</button>
+		  </div>
+		</div><!-- end main -->
+
+    <!-- The first section div -->
+		<div class="section content">
+		  <div class="container">
+			<div class="row">
+				<div class="col-md-6">
+				<img src="iphone.jpeg">
+				</div>
+				<div class="col-md-6">
+				<h1>
+				  Share
+				</h1>
+				<p>
+				  Share your picture with the people who matter most. With Foto, it's easy to share with the right people, anytime.
+				</p>
+				</div> <!-- end col-md-6 -->
+			</div> <!-- end row -->
+		  </div> <!-- end container -->
+		</div> <!-- end section -->
+	
+    <!-- The second section div -->
+		<div class="section content">
+		  <div class="container">
+			<div class="row">
+			  <div class="col-md-6">
+				<h1>
+				  Explore
+				</h1>
+				<p>
+				  Explore moments from your friends and family. Foto makes it easy to see what's happening in the world and take in everything around you.
+				</p>
+			  </div>
+			  <div class="col-md-6">
+				<img src="outdoorphone.jpeg">
+			  </div> <!-- end col-md-6 -->
+			</div> <!-- end row -->
+		  </div> <!-- end container -->
+		</div> <!-- end section -->
+
+    <!-- The third section div -->
+    <div class="section content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="skylinephone.jpeg">
+          </div>
+          <div class="col-md-6">
+            <h1>
+              Save
+            </h1>
+            <p>
+              Save every photo you take. Foto automatically saves your photos so you can show them off from anywhere.
+            </p>
+          </div> <!-- end col-md-6 -->
+        </div> <!-- end row -->
+      </div> <!-- end container -->
+    </div> <!-- end section -->
+
+    
+    <div class="store section container">
+      <h2>Available for iPhone, iPad, and Android.</h2>
+      <img src="https://s3.amazonaws.com/codecademy-content/projects/shutterbugg/app-store.png" width="185px" />
+      <img alt="Get it on Google Play" src="https://s3.amazonaws.com/codecademy-content/projects/shutterbugg/google-play.png" width="159px" />
+    </div>
+
+    <!-- The footer div -->
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <h1>
+              Company
+            </h1>
+            <ul>
+              <li><a href="Careers/index.php">Careers</a></li>
+              <li><a href="About/index.php">About</a></li>
+              <li><a href="Contact/index.php">Contact</a></li>
+            </ul>
+          </div>
+           <div class="col-md-3">
+              <h1>
+                Products
+              </h1>
+              <ul>
+                <li><a href="#">Foto</a></li>
+                <li><a href="#">Lenscape</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <h1>
+                News
+              </h1>
+              <ul>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Youtube</a></li>
+                <li><a href="#">Google+</a></li>
+                <li><a href="#">Facebook</a></li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div> <!-- end footer -->
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
