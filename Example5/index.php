9b25bed3d5ba457c0ab5c9d20e3bc1dd3ecc35db
kburthey <kburthey@gmail.com>
2017-11-10 10:55:21 -0500

    Add files via upload

---
+++ Example5/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+  <head>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://s3.amazonaws.com/codecademy-content/projects/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="pocketbook.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="container">
+        <img src="https://s3.amazonaws.com/codecademy-content/projects/pocketbook/logo.svg" class="logo" />
+				
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <h2>
+              Join Pocketbook
+            </h2>
+            <form role="form">
+              <div class="form-group">
+                <label for="exampleinputname">First Name</label>
+                <input type="text" class="form-control" id="exampleinputname" placeholder="">
+              </div>
+              <div class="form-group">
+                <label for="exampleinputlastname">Last Name</label>
+                <input type="text" class="form-control" id="exampleinputlastname" placeholder="">
+              </div>
+              <div class="form-group">
+                <label for="exampleemail">Email</label>
+                <input type="email" class="form-control" id="exampleemail" placeholder="">
+              </div>
+              <div class="form-group">
+                <label for="examplepassword">Password</label>
+                <input type="password" class="form-control" id="examplepassword" placeholder="">
+              </div>
+              <button type="submit" class="btn btn-default">
+                Create account
+              </button>
+            </form>
+          </div>
+        </div>
+			
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <ul class="nav">
+            <li>Contact</li>
+            <li>Help Center</li>
+            <li>About</li>
+          </ul>
+        </div>
+			
+      </div>
+    </div>
+  </body>
+</html>
